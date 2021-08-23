89223235d09c4370e8724037a96e376b24200f55
yparam98 <yathavanparamesh1998@gmail.com>
2021-08-22 23:58:04 -0400

    adjusted formula

--- rectangle_area_ii.py
+++ rectangle_area_ii.py
@@ -3,21 +3,26 @@ from typing import List
 
 class Solution:
     def rectangleArea(self, rectangles: List[List[int]]) -> int:
-        # total_area = 0
+        total_area: int = 0
 
-        # for rectangle in rectangles:
-        #     print(total_area)
-        #     total_area += rectangle[0]*rectangle[1]*rectangle[2]*rectangle[3]
-
-        # return total_area
-        return rectangles
+        for rectangle in rectangles:
+            base: int = rectangle[2]-rectangle[0]
+            height: int = rectangle[3]-rectangle[1]
+            area: int = base*height
+        
+            print('base: {0}, height: {1}, area: {2}'.format(base, height, area))
+            
+            total_area += area
+            
+        return total_area
 
 def main():
     solution = Solution()
     rectangles: List[int] = []
-    for i in range(4):
+    num_rects: int = int(input('how many rectangles? '))
+    for i in range(num_rects):
         rectangle: List[int] = []
-        for val in input('enter 4 coords: ').split(' '):
+        for val in input('x1 y1 x2 y2: ').split(' '):
             rectangle.append(int(val))
         rectangles.append(rectangle)
     print(solution.rectangleArea(rectangles))
